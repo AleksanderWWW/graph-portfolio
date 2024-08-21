d385f14a07d6617a9a53fc92ddff286854bc7240
AleksanderWWW <alwojnarowicz@gmail.com>
2024-08-21 15:27:24 +0200

    fix path for docker

--- deploy/ecr/push_image.sh
+++ deploy/ecr/push_image.sh
@@ -11,5 +11,5 @@ fi
 echo "Pushing docker image to ${REPO_URL} with tag ${TAG}"
 
 aws ecr get-login-password | docker login --username AWS --password-stdin "${REPO_URL}"
-docker build --platform linux/amd64 -t "${REPO_URL}":"${TAG}" .
+docker build --platform linux/amd64 -t "${REPO_URL}":"${TAG}" ../..
 docker push "${REPO_URL}":"${TAG}"
