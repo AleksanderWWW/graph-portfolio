9c9fc9c48d66cc1da3a6a55fbe9be15d64dee26a
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-02-06 19:53:05 +0100

    update

--- src/quantum_portfolio/graph.py
+++ src/quantum_portfolio/graph.py
@@ -1,13 +1,26 @@
-import pandas as pd
 import networkx as nx
+import pandas as pd
+from networkx.algorithms.approximation import maximum_independent_set
 
 
-def build_graph(data: pd.DataFrame, corr_threshold: float) -> nx.Graph:
-    corr_matrix = data.pct_change().corr()
-
+def get_max_diversified_assets(
+    data: pd.DataFrame, corr_threshold: float = 0.5
+) -> set[str]:
+    corr_matrix = get_corr_matrix(data)
     label_mapping = {i: name for i, name in enumerate(corr_matrix.columns)}
 
-    graph = nx.from_numpy_array(((corr_matrix.values > corr_threshold) & (corr_matrix.values < 1)))
-    nx.set_node_attributes(graph, label_mapping, "label")
+    graph = build_graph(corr_matrix, corr_threshold)
 
-    return graph
+    max_ind = maximum_independent_set(graph)
+
+    return set(label_mapping[label] for label in max_ind)
+
+
+def build_graph(corr_matrix: pd.DataFrame, corr_threshold: float) -> nx.Graph:
+    return nx.from_numpy_array(
+        ((corr_matrix.values > corr_threshold) & (corr_matrix.values < 1))
+    )
+
+
+def get_corr_matrix(data: pd.DataFrame) -> pd.DataFrame:
+    return data.pct_change().corr()
--- src/quantum_portfolio/readers/stooq_reader.py
+++ src/quantum_portfolio/readers/stooq_reader.py
@@ -1,11 +1,11 @@
-import datetime
 import asyncio
-import aiohttp
+import datetime
 import os
 from enum import Enum
 from io import StringIO
 from urllib.parse import urlencode, urlunparse
 
+import aiohttp
 import pandas as pd
 
 from quantum_portfolio.utils import URLComponents
@@ -31,9 +31,8 @@ RETURN_COLUMNS_NAME: str = "Zamkniecie"
 def read_stooq(
     tickers: list[str], *, start_date: datetime.date, end_date: datetime.date
 ) -> pd.DataFrame:
-     return asyncio.run(read_stooq_data(
-        tickers, start_date=start_date, end_date=end_date
-        )
+    return asyncio.run(
+        read_stooq_data(tickers, start_date=start_date, end_date=end_date)
     )
 
 
@@ -41,7 +40,7 @@ async def read_stooq_data(
     tickers: list[str], *, start_date: datetime.date, end_date: datetime.date
 ) -> pd.DataFrame:
     """Fetch stock data from Stooq asynchronously for multiple tickers."""
-    
+
     query_params = {
         "d1": start_date.strftime(DATE_FORMAT),
         "d2": end_date.strftime(DATE_FORMAT),
@@ -51,27 +50,35 @@ async def read_stooq_data(
     urls = [
         urlunparse(
             URLComponents(
-                scheme="https", 
-                netloc=HOST, 
-                url="/q/d/l", 
-                path="", 
+                scheme="https",
+                netloc=HOST,
+                url="/q/d/l",
+                path="",
                 query=urlencode(query_params | {"s": ticker}),
-                fragment="")
+                fragment="",
+            )
         )
         for ticker in tickers
     ]
 
     async with aiohttp.ClientSession() as session:
-        tasks = [read_single_ticker(session, url, ticker) for url, ticker in zip(urls, tickers)]
+        tasks = [
+            read_single_ticker(session, url, ticker)
+            for url, ticker in zip(urls, tickers)
+        ]
         results = await asyncio.gather(*tasks)
 
     return pd.concat(results, axis=1)
 
 
-async def read_single_ticker(session: aiohttp.ClientSession, url: str, ticker: str) -> pd.DataFrame:
+async def read_single_ticker(
+    session: aiohttp.ClientSession, url: str, ticker: str
+) -> pd.DataFrame:
     """Fetch a single ticker asynchronously and parse the data."""
     async with session.get(url) as response:
         text = await response.text()
 
+    if INDEX_COLUMN_NAME not in text:
+        raise ValueError(f"No data found for ticker '{ticker}' [URL: {url}]")
     data = pd.read_csv(StringIO(text), index_col=INDEX_COLUMN_NAME)
-    return data[[RETURN_COLUMNS_NAME]].rename(columns={RETURN_COLUMNS_NAME: ticker})    
+    return data[[RETURN_COLUMNS_NAME]].rename(columns={RETURN_COLUMNS_NAME: ticker})
