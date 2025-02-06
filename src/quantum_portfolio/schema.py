4464942fc251e542b2ce0c8bd76ecdf4afa872de
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-02-06 20:23:12 +0100

    litserve prep

--- src/quantum_portfolio/schema.py
+++ src/quantum_portfolio/schema.py
@@ -1,18 +1,23 @@
 import datetime
-from dataclasses import dataclass
+
+from typing import Self
+from pydantic import BaseModel, Field, ValidationError, model_validator
 
 
-@dataclass
-class QueryData:
+class QueryData(BaseModel):
     start: datetime.date
     end: datetime.date
-    tickers: list[str]
+    tickers: list[str] = Field(..., min_items=1, description="List of stock tickers")
+
+    @model_validator(mode="after")
+    def validate_date_range(self) -> Self:
+        """Ensure the end date is after the start date."""
+        if self.end < self.start:
+            raise ValidationError("End date must be after the start date.")
+        return self
 
 
-@dataclass
-class Response:
+class Response(BaseModel):
     tickers: list[str]
-    execution_time_seconds: int
-    annealer_run_time_seconds: int
     is_independent_set: bool
     errors: list[str] | None = None
