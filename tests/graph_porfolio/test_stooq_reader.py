34f0bfe198b391aa0fa480385e3d895e6685e3eb
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-02-12 20:30:58 +0100

    add tests

---
+++ tests/graph_porfolio/test_stooq_reader.py
@@ -0,0 +1,23 @@
+import datetime
+from datetime import timedelta
+from unittest.mock import patch, Mock
+
+import pytest
+
+from graph_portfolio.stooq_reader import read_stooq
+
+
+@pytest.mark.unit
+@patch("graph_portfolio.stooq_reader.asyncio.run")
+def test_read_stooq_is_cached(mock_asyncio_run: Mock):
+    start = datetime.date.today() - timedelta(days=365)
+    end = datetime.date.today()
+
+    for _ in range(10):
+        read_stooq(
+            tickers=frozenset(["ticker1", "ticker2", "ticker3"]),
+            start_date=start,
+            end_date=end,
+        )
+
+    assert mock_asyncio_run.call_count == 1
